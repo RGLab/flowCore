febf16f3f95f6b9a7aeca28f496ebc89b995dc8f
nlemeur@fhcrc.org <nlemeur@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-12-01 18:08:58 +0000

    modify the other transformation functions
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@21330 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/transformation-functions.R
+++ R/transformation-functions.R
@@ -7,131 +7,41 @@ linearTransform <- function(transformationId,a,b){
     }
 }
 
-
-## ==========================================================================
-## Linear transformation function
-## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-##linearTransformation <- function(transformation,flowObject){
-##    parameters=transformation@parameters
-##    a=transformation@a
-##   b=transformation@b
-##    ## test for validity of objects
-##    if(class(flowObject)!="flowFrame"){
-##        stop("flowObject must of class flowFrame.")
-##    }
-##    if (is.null(flowObject@exprs)) {
-##        stop("There is no data to createFilter.")
-##    }
-##    data <- flowObject@exprs
-##    flowObject@exprs[,parameters] <- a * data[,parameters] + b
-##    flowObject@description[["transformation"]] <- paste("A linear transformation was applied on the",
-##                                                     parameters, "channel",sep="",collapse=" ")
-##    
-##    return(flowObject)
-##}
-
 ## ==========================================================================
 ## Quadratic transformation function
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-quadraticTransformation <- function(transformation,flowObject){
-    parameters=transformation@parameters
-    a=transformation@a
-    b=transformation@b
-    c=transformation@c
-    ## test for validity of objects
-    
-    if(class(flowObject)!="flowFrame"){
-        stop("flowObject must of class flowFrame.")
+quadraticTransform <- function(transformationId,a,b,c){
+    function(x){
+        x <- a*x^2 + b*x + c
     }
-    if (is.null(flowObject@exprs)) {
-        stop("There is no data to createFilter.")
-    }
-    data <- flowObject@exprs
-    flowObject@exprs[,parameters] <- a * data[,parameters]^2 + b * data[,parameters] + c
-    flowObject@description[["transformation"]] <- paste("A quadratic transformation was applied on the",
-                                                     parameters, "channel",sep="",collapse=" ")
-    
-    return(flowObject)
 }
 
 ## ==========================================================================
 ## Natural logarithm transformation function
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-lnTransformation <- function(transformation,flowObject){
-    parameters=transformation@parameters
-    r=transformation@r
-    d=transformation@d
-   
-    ## test for validity of objects
-    if(class(flowObject)!="flowFrame"){
-        stop("flowObject must of class flowFrame.")
-    }
-    if (is.null(flowObject@exprs)) {
-        stop("There is no data to createFilter.")
-    }
-
-    data <- flowObject@exprs
-    flowObject@exprs[,parameters] <- log(data[,parameters])*(r/d)
-    flowObject@description[["transformation"]] <- paste("A natural logarithm transformation was applied on the",
-                                                     parameters, "channel",sep="",collapse=" ")
-    
-    return(flowObject)
+lnTransform <- function(transformationId,r,d){
+    function(x){
+     x<-log(x)*(r/d)
+ }
 }
-    
-    
+
 ## ==========================================================================
 ## Logarithm transformation function
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-logTransformation <- function(transformation,flowObject){
-    parameters=transformation@parameters
-    r=transformation@r
-    d=transformation@d
-    logbase=transformation@logbase
-   
-    ## test for validity of objects
-    if(class(flowObject)!="flowFrame"){
-        stop("flowObject must of class flowFrame.")
+logTransform <- function(transformationId,logbase,r,d){
+    function(x){
+        x <- log(x,logbase)*(r/d)
     }
-    if (is.null(flowObject@exprs)) {
-        stop("There is no data to createFilter.")
-    }
-    
-    data <- flowObject@exprs
-    flowObject@exprs[,parameters] <- log(data[,parameters],base=logbase)*(r/d)
-    flowObject@description[["transformation"]] <- paste("A log transformation was applied on the",
-                                                     c(parameters), "channel",sep="",collapse=" ")
-    
-    return(flowObject)
 }
-    
 
 ## ==========================================================================
 ## General biexponential transformation function
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-biexponentialTransformation <- function(transformation,flowObject) {
-  parameters=transformation@parameters
-  a=transformation@a
-  b=transformation@b
-  c=transformation@c
-  d=transformation@d
-  f=transformation@f
-  w=transformation@w
-  tol=transformation@tol
-  maxit=transformation@maxit
-  
-  ## test for validity of objects
-  if(class(flowObject)!="flowFrame"){
-    stop("flowObject must of class flowFrame.")
-  }
-  if (is.null(flowObject@exprs)) {
-    stop("There is no data to createFilter.")
-  }
-  flowObject@exprs[,parameters] <- .Call(biexponential_transform,flowObject@exprs[,parameters],a,b,c,d,f,w,tol,maxit)
-  flowObject@description[["transformation"]] <- paste("A biexponential transformation was applied on the",
-                                                   c(parameters), "channel",sep="",collapse=" ")
-  return(flowObject)
+biexponentialTransform<- function(transformationId,a,b,c,d,f,w,tol,maxit){
+    function(x){
+        x <- .Call(biexponential_transform,x,a,b,c,d,f,w,tol,maxit)
+    }
 }
-
 
 logicleTransform <- function(w=0,r=262144,d=5,...) {
   if(w>d) stop("Negative range decades must be smaller than total number of decades")
