5bfc566cefb96b48559449979bd00bca5a39c57b
Nolwenn Lemeur <nlemeur@fhcrc.org>
2006-12-01 18:08:58 +0000

    modify the other transformation functions
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@21330 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/transformation-methods.R
+++ R/transformation-methods.R
@@ -1,98 +1,3 @@
-
-## ==========================================================================
-## Apply linear transformation on fcsframe Object
-## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-##setMethod("applyTransformation",
-##          signature=signature(transformation="linearTransformation",flowObject="flowFrame"),
-##          definition=function(transformation,flowObject) {
-##            transformNew <-linearTransformation(transformation,flowObject)
-##             msg <- paste("Linear transformation applied on ",
-##                           deparse(substitute(flowset)),
-###                           " (file:",deparse(substitute(flowObject)),
-##                           ") a ", class(flowObject)," object", sep="")
-##            print(msg)
-##            return(transformNew)
-##         }
-##          
-## )
-
-## ==========================================================================
-## Apply quadratic transformation on flowFrame Object
-## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-##setMethod("applyTransformation",
-##          signature=signature(transformation="quadraticTransformation",flowObject="flowFrame"),
-##          definition=function(transformation,flowObject) {
-##            transformNew <-quadraticTransformation(transformation,flowObject)
-##             msg <- paste("Quadratic transformation applied on ",
-##                           deparse(substitute(flowObject)),
-##                           " (file:",deparse(substitute(flowObject)),
-##                           ") a ", class(flowObject)," object", sep="")
-##            print(msg)
-##            return(transformNew)
-##          }
-##          
-## )
-
-## ==========================================================================
-## Apply LN transformation on flowFrame Object
-## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-##setMethod("applyTransformation",
-##          signature=signature(transformation="lnTransformation",flowObject="flowFrame"),
-##          definition=function(transformation,flowObject) {
-##            transformNew <-lnTransformation(transformation,flowObject)
-##             msg <- paste("Natural Logarithmical (LN) transformation applied on ",
-##                           deparse(substitute(flowObject)),
-##                           " (file:", deparse(substitute(flowObject)),
-##                          ") a ", class(flowObject)," object", sep="")
-##            print(msg)
-##            return(transformNew)
-##          }
-       
-## )
-## ==========================================================================
-## Apply LOG transformation on FCS Object
-## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-##setMethod("applyTransformation",
-##          signature=signature(transformation="logTransformation",flowObject="flowFrame"),
-##          definition=function(transformation,flowObject) {
-##            transformNew <-logTransformation(transformation,flowObject)
-##             msg <- paste("Logarithmical transformation applied on ",
-##                           deparse(substitute(flowObject)),
-##                           " (file:", deparse(substitute(flowObject)),
-##                           ") a ", class(flowObject)," object", sep="")
-##            print(msg)
-##            return(transformNew)
-##          }
-##          
-## )
-## ==========================================================================
-## Apply HyperLog transformation on FCS Object
-## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-##setMethod("applyTransformation",
-##          signature=signature(transformation="hyperlogTransformation",flowObject="flowFrame"),
-##          definition=function(transformation,flowObject) {
-##            transformNew <-hyperlogTransformation(transformation,flowObject)
-##             msg <- paste("hyperlog transformation applied on ",
-##                           deparse(substitute(flowObject)),
-##                           " (file:", deparse(substitute(flowObject)),
-##                           ") a ", class(flowObject)," object", sep="")
-##            print(msg)
-##            return(transformNew)
-##          }
-##          
-## )
-
-## ==========================================================================
-## Apply biexponential transformation on FCS Object
-## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-##setMethod("applyTransformation",
-##          signature=signature(transformation="biexponentialTransformation",flowObject="flowFrame"),
-##          definition=function(transformation,flowObject) {
-##            transformNew <- biexponentialTransformation(transformation,flowObject)
-##            return(transformNew)
-##          }
-##          )
-
 ## ==========================================================================
 ## Transformation function for flowFrame
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -100,8 +5,8 @@ setMethod("transform",
           signature=signature(`_data`="flowFrame"),
           definition=function(`_data`, ...) {
               flowObject <- `_data`
-              e <- substitute(...)
-              transformed <- transform(flowObject@exprs,...)
+              e <- substitute(list(...))
+              transformed <- transform(as.data.frame(flowObject@exprs),...)
               newFlowFrame <- new("flowFrame", exprs=as.matrix(transformed),
                                   description=flowObject@description)
               newFlowFrame@description <- c(newFlowFrame@description,"transformation"=e)
