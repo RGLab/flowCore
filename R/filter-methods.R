e7171a7fb9ffaf25cfcddb6f7c3af9fb1607e8af
ellis <ellis@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-12-10 04:56:09 +0000

    * Added a summary method primarily aimed at filterResult (and compositions thereof), including a special one for subsetFilters. * Added a print method (S3) for the summaryResult so that something sensible is displayed despite using the usual '$' operator to access elements. * Fixed some stupidity in the construction of parameter lists. I don't know why I didn't just use unique(). I blame eggnog. :-) * Updated even more of the filterTest.R test file. It now gets past Perry's example1.r because I have access to not gates (among other things). Shows the new subset syntax and the summary() method in action.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@21415 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/filter-methods.R
+++ R/filter-methods.R
@@ -138,8 +138,7 @@ setMethod("%in%",signature("flowFrame",table="norm2Filter"),function(x,table) {
 ## The | operator returns an object representing the union of two filters. A simple
 ## optimization would be to linearize the union of a filter and another union filter.
 setMethod("|",signature("filter","filter"),function(e1,e2) {
-	p1 = sapply(e1@parameters,"grep",x=e2@parameters)	
-	new("unionFilter",parameters=c(e1@parameters[-p1],e2@parameters),filters=list(e1,e2),filterId=paste(e1@filterId,"or",e2@filterId))
+	new("unionFilter",parameters=unique(c(e1@parameters,e2@parameters)),filters=list(e1,e2),filterId=paste(e1@filterId,"or",e2@filterId))
 })
 ## A union filter returns TRUE if ANY of the argument filters return true.
 setMethod("%in%",c("flowFrame","unionFilter"),function(x,table) {	
@@ -150,8 +149,7 @@ setMethod("%in%",c("flowFrame","unionFilter"),function(x,table) {
 ## is somewhat different from the %subset% operator because some filters depend on the 
 ## data and would return different results when applied to the full dataset.
 setMethod("&",signature("filter","filter"),function(e1,e2) {
-	p1 = sapply(e1@parameters,"grep",x=e2@parameters)
-	new("intersectFilter",parameters=c(e1@parameters[-p1],e2@parameters),filters=list(e1,e2),filterId=paste(e1@filterId,"and",e2@filterId))
+	new("intersectFilter",parameters=unique(c(e1@parameters,e2@parameters)),filters=list(e1,e2),filterId=paste(e1@filterId,"and",e2@filterId))
 })
 ## An intersectFilter only returns TRUE if ALL the member filters are TRUE.
 setMethod("%in%",c("flowFrame","intersectFilter"),function(x,table) {
@@ -167,8 +165,7 @@ setMethod("%in%",c("flowFrame","complementFilter"),function(x,table) !(x%in%tabl
 ## The %subset% operator constructs a filter that takes the subset of the LHS filter in the RHS filter result. 
 ## For many cases this is equivalent to an intersection filter.
 setMethod("%subset%",signature("filter","filter"),function(e1,e2) {
-	p1 = sapply(e1@parameters,"grep",x=e2@parameters)
-	new("subsetFilter",parameters=c(e1@parameters[-p1],e2@parameters),left=e1,right=e2,filterId=paste(e1@filterId,"in",e2@filterId))
+	new("subsetFilter",parameters=unique(c(e1@parameters,e2@parameters)),left=e1,right=e2,filterId=paste(e1@filterId,"in",e2@filterId))
 })
 ## Returns TRUE for elements that are true on the LHS and the RHS, however the LHS filter is only executed against the subset returned
 ## by the RHS filtering operation. This is particularly important for unsupervised filters like norm2Filter. The result is still relative
@@ -195,6 +192,24 @@ setMethod("%in%",c("flowFrame","filterResult"),function(x,table) {
 setAs("filter","logical",function(from) stop("Only resolved filters can be converted to a logical vector."))
 setAs("filterResult","logical",function(from) if(is.logical(from@subSet)) from@subSet else from@subSet==1)
 setAs("subsetFilter","logical",function(from) as(from@left,"logical") & as(from@right,"logical"))
-setAs("intersectFilter","logical",function(from) apply(sapply(table@filters,as,Class="logical"),1,all))
-setAs("unionFilter","logical",function(from) apply(sapply(table@filters,as,Class="logical"),1,any))
+setAs("intersectFilter","logical",function(from) apply(sapply(from@filters,as,Class="logical"),1,all))
+setAs("unionFilter","logical",function(from) apply(sapply(from@filters,as,Class="logical"),1,any))
 setAs("complementFilter","logical",function(from) !as(from@filter,"logical"))
+
+
+setMethod("summary","filter",function(object,...) {
+	l = as(object,"logical")
+	true = sum(l)
+	count= length(l)
+	structure(list(true=true,false=count-true,n=count,p=true/count,q=1-(true/count),name=object@filterId),class="filterSummary")
+})
+setMethod("summary","subsetFilter",function(object,...) {
+	e1 = as(object@left,"logical")
+	e2 = as(object@right,"logical")
+	true = sum(e1&e2)
+	count= sum(e2)
+	structure(list(true=true,false=count-true,n=count,p=true/count,q=1-(true/count),name=object@filterId),class="filterSummary")	
+})
+print.filterSummary <- function(x,...) {
+	with(x,cat(sprintf("%s: %d of %d (%.2f%%)\n",name,true,n,100*p)))
+}
