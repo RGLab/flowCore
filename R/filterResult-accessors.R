e3e2f96791285104be06e7f89f363e03167b777b
fhahne <fhahne@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-12-21 11:51:47 +0000

    some code formating
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@21633 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/filterResult-accessors.R
+++ R/filterResult-accessors.R
@@ -10,14 +10,17 @@ setAs("randomFilterResult","logical",function(from) runif(length(from@subSet))<f
 
 ##Allow us to compare filterResults and flowFrames. This lets us check (and warn or stop)
 ##that a particular flowFrame generated a filterResult allowing us to use it for further processing.
-setMethod("identifier","filterResult",function(object) object@frameId)
-setMethod("==",signature("flowFrame","filterResult"),function(e1,e2) {
-	i1 = identifer(e1)
-	i2 = identifier(e2)
-	(length(i1) == 0 || length(i2) == 0 || i1 == i2)
-})
+setMethod("identifier", signature="filterResult",
+          definition=function(object) object@frameId)
+setMethod("==",signature("flowFrame","filterResult"),
+          definition=function(e1,e2) {
+            i1 = identifer(e1)
+            i2 = identifier(e2)
+            (length(i1) == 0 || length(i2) == 0 || i1 == i2)
+          })
 #Does S4 do this for us automagically? I don't know!
-setMethod("==",signature("filterResult","flowFrame"),function(e1,e2) e2==e1)
+setMethod("==",signature("filterResult","flowFrame"),
+          definition=function(e1,e2) e2==e1)
 
 
 
