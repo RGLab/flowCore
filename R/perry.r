66e144c204430dda5d69c08c7a5740c0489b8e41
Perry_D_Haaland@bd.com <Perry_D_Haaland@bd.com@bc3139a8-67e5-0310-9ffc-ced21a209358>
2007-01-02 21:09:26 +0000

    I moved my plot method to the file perry.r and renamed it "flowplot". I'll move this specialized convenience function to a local package before we distribute flowCore, but I'm going to keep it here temporarily for testing purposes.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@21748 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/perry.r
+++ R/perry.r
@@ -1,24 +1,99 @@
-## last changed November 2, 2006
-##   I abstracted out the code for checking whether or not the parent exists
+## last changed January 2, 2007
+##   
+## I moved my plot method here as it was getting overwritten by the core plot method
+## if the consensus is that this kind of convenience function doesn't belong in the core 
+## package, I'll take it local
+## Last changed November 3, 2006
+##  pdh -- fixed an error because I wasn't checking for xlim and ylim when y was missing.
 
-##getParentSet = function(parent,ncells) {
-## 	if(missing(parent)) {
-   		## if the parent is missing assume that it is all cells
-##		parentSet = rep(1,ncells)
-## 	}
-##	else {  	
-##		if(class(parent) != "filterResult") {
-##			stop("parent must be of class filterResult.")
-##		}
-##		else {
-##			if(is.null(parent@subSet)) {
-##				stop("The filterResult parent must have a subSet slot.")
-##			}
-##			else {
-##				parentSet = parent@subSet
-##			}  	
-##		}
-##	}
-##	parentSet
-##}
- 
+## ==========================================================================
+## Basic plot for fcsFrame object
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+setGeneric("flowplot",function(x,...) standardGeneric("flowplot"))
+setMethod("flowplot",
+          ## basic plot without a gate specified
+          signature(x="flowFrame"),
+          function(x, y, filter=NULL, plotParameters=c("FSC-H","SSC-H"),parent,colParent="Grey",colSubSet="Blue",xlab,ylab,xlim,ylim,...){
+            data <- x@exprs
+            ncells <- nrow(data)
+            if(missing(xlab)){
+              if(is.character(plotParameters)){
+                xlab <-  plotParameters[1]
+              }
+              else {
+                xlab <-  colnames(data)[plotParameters[1]]
+              }
+            }
+            if(missing(ylab)){
+              if(is.character(plotParameters)){
+                ylab <-  plotParameters[2]
+              }
+              else {
+                ylab <-  colnames(data)[plotParameters[2]]
+              }
+            }
+            if(missing(y)) {
+              ## check for the parent. If it is a filterResult get the parentSet
+              parentSet <-  getParentSet(parent,ncells)
+              selectParent <- (parentSet==1)
+              if(missing(xlim)) {
+                xlim <- range(data[selectParent,plotParameters[1]])
+              }
+              if(missing(ylim)) {
+                ylim <-  range(data[selectParent,plotParameters[2]])
+              }
+              
+              smoothScatter(data[selectParent,plotParameters[1]],data[selectParent,plotParameters[2]],nrpoints=50,
+                            xlim=xlim,
+                            ylim=ylim,
+                            xlab=xlab,
+                            ylab=ylab,
+                            ...)       
+            }
+            else {
+              ## check for y and if it is a filterResult, get the subSet
+              if(class(y) != "logicalFilterResult") {
+                stop("y must be of class filterResult.")
+              }
+              else {
+                if(is.null(y@subSet)) {
+                  stop("The filterResult y must have a subSet slot.")
+                }
+                else {
+                  subSet <-  y@subSet
+                }
+              }
+              ## check for the parent. If it is a filterResult get the parentSet
+              parentSet <- getParentSet(parent,ncells)
+              
+              ## check for the proper relationship between the parent and child
+              if(any(subSet>parentSet)) {
+                stop("The child population must be contained in the parent population.")
+              }
+              selectParent <-  (parentSet==1)
+              selectSubSet <-  (subSet==1)
+              selectUnfiltered <- ((parentSet-subSet) ==1)
+              if(missing(xlim)) {
+                xlim = range(data[selectParent,plotParameters[1]])
+              }
+              if(missing(ylim)) {
+                ylim = range(data[selectParent,plotParameters[2]])
+              }
+              
+              plot(data[selectSubSet,plotParameters[1]],data[selectSubSet,plotParameters[2]],
+                   xlab=xlab,
+                   ylab=ylab,
+                   xlim=xlim,
+                   ylim=ylim,
+                   type="n",
+                   ...)
+              ## filtered population
+              points(data[selectSubSet,plotParameters[1]],data[selectSubSet,plotParameters[2]],col=colSubSet,...)
+              ## unfiltered population
+              points(data[selectUnfiltered,plotParameters[1]],data[selectUnfiltered,plotParameters[2]],col=colParent,...)
+              if(length(filter)!=0){
+                ln <- filter@boundaries
+                lines(rbind(ln,ln[1,]),col="green",lwd=2)  
+              }
+            }
+          })
