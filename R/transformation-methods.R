d9f275f4df847f8b210edd56643e772c3aff86b0
nlemeur@fhcrc.org <nlemeur@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2007-01-18 01:23:29 +0000

    modification of the method will probably need more work
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@22053 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/transformation-methods.R
+++ R/transformation-methods.R
@@ -1,21 +1,28 @@
+## NLM Jan 17
 ## ==========================================================================
 ## Transformation function for flowFrame
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 setMethod("transform",
           signature=signature(`_data`="flowFrame"),
           definition=function(`_data`, ...) {
-			e = substitute(list(...))
-			transformed = as.matrix(transform(as.data.frame(exprs(`_data`)),...))
-			#Add any new parameter values
-			param.names = colnames(transformed)
-			newParams   = is.na(match(param.names,`_data`@parameters$name))
-			params      = parameters(`_data`)
-			if(any(newParams)) {
-				params = rbind(params,
-					data.frame(name=param.names[newParams]))
-			}
-			new("flowFrame",
-				exprs=transfomed[,params$name],
-				parameters=params,
+			e <- substitute(list(...))
+                        transformed <- as.matrix(transform(as.data.frame(exprs(`_data`)),...))
+                        ##Add any new parameter values
+			param.names <- colnames(transformed)
+                        newParams <- is.na(match(param.names,`_data`@parameters$name))
+			#params      = parameters(`_data`)
+                        params <- `_data`@parameters$name
+                        if(any(newParams)) {
+				params <- cbind(params,
+                                  data.frame(name=param.names))
+                                ##params = cbind(params,
+                                ##data.frame(name=param.names[newParams]))
+                            }
+                        colnames(transformed) <- `_data`@parameters$name
+                        new("flowFrame",
+				##exprs=transformed[,params$name],
+                                exprs=transformed,
+				parameters=`_data`@parameters,
 				description=description(`_data`))
           })
+ 
