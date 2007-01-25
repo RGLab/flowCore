6d98f6f355cd0e3af5ae0377c5e665bccc521c83
Nolwenn Lemeur <nlemeur@fhcrc.org>
2007-01-25 23:43:26 +0000

    transformation method fix man page update
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@22213 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/transformation-methods.R
+++ R/transformation-methods.R
@@ -5,30 +5,28 @@
 setMethod("transform",
           signature=signature(`_data`="flowFrame"),
           definition=function(`_data`, ...) {
-			e <- substitute(list(...))
-                        transformed <- as.matrix(transform(as.data.frame(exprs(`_data`)),...))
-                        ##Add any new parameter values
-			param.names <- colnames(transformed)
-                        newParams <- is.na(match(param.names,`_data`@parameters$name))
-			params      = parameters(`_data`)$name
-                        if(any(newParams)) {
-				params <- cbind(params,
+              e <- substitute(list(...))
+              transformed <- as.matrix(transform(as.data.frame(exprs(`_data`)),...))
+              ##Add any new parameter values
+              param.names <- colnames(transformed)
+              newParams <- is.na(match(param.names,`_data`@parameters$name))
+              params      = parameters(`_data`)$name
+              if(any(newParams)) {
+                  params <- cbind(params,
                                   data.frame(name=param.names))
-                                ##params = cbind(params,
-                                ##data.frame(name=param.names[newParams]))
-                            }
-                        colnames(transformed) <- parameters(`_data`)$name
-                        new("flowFrame",
-				##exprs=transformed[,params$name],
-                                exprs=transformed,
-				parameters=parameters(`_data`),
-				description=description(`_data`))
+                  ##params = cbind(params,
+                  ##    data.frame(name=param.names[newParams]))
+              }
+              colnames(transformed) <- parameters(`_data`)$name
+              new("flowFrame",
+                  exprs=transformed[,params$name],
+                  parameters=parameters(`_data`),
+                  description=description(`_data`))
           })
 ## ==========================================================================
 ## Transform function for flowSet
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 setMethod("transform",signature=signature(`_data`="flowSet"),function(`_data`,...) {
-    ##`_data`=x
     x = `_data`
     y = as(structure(lapply(seq(along=x),
       function(i) transform(`_data`=x[[i]],...)),names=sampleNames(phenoData(x))),"flowSet")
