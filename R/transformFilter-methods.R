e42473fb4d02a5ee134341cbcd99d8f862f41c5b
Byron Ellis <ellis@stat.harvard.edu>
2007-02-12 19:37:47 +0000

    Small changes to properly export transformFilters. Also, filters with more than one parameter are now generating appropriate names.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@22592 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/transformFilter-methods.R
+++ R/transformFilter-methods.R
@@ -1,7 +1,7 @@
 ## Constructors
 setMethod("%on%",signature("filter","transformList"),function(e1,e2) {
 	new("transformFilter",
-		filterId=paste(e1@filterId,"on transformed values of",paste(colnames(e2),sep=",")),
+		filterId=paste(e1@filterId,"on transformed values of",paste(colnames(e2),collapse=",")),
 		transforms=e2,
 		filter=e1,parameters=e1@parameters)
 })
