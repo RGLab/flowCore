7e89ab194589fa951ba393889e4da882b7abd62e
Florian Hahne <f.hahne@dkfz-heidelberg.de>
2007-02-19 18:08:42 +0000

    added %in% method for ellipsoidal gates (still a bit slow...)
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@22691 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/ellipsoidGate-accessors.R
@@ -0,0 +1,12 @@
+## ==========================================================================
+## filter flowFrame object using ellipsoidGate
+## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+setMethod("%in%",signature(x="flowFrame",table="ellipsoidGate"),function(x,table) {
+  e <- if(length(table@parameters)==1) as.matrix(exprs(x)[,table@parameters]) else
+          exprs(x)[,table@parameters]
+  d1 <- apply(sapply(seq(along=table@parameters), function(i) (e[,i] -
+                           table@focus[1,i])^2), 1, function(k) sqrt(sum(k)))
+  d2 <- apply(sapply(seq(along=table@parameters), function(i) (e[,i] -
+                           table@focus[2,i])^2), 1, function(k) sqrt(sum(k)))
+  (d1+d2) <= table@distance
+})
