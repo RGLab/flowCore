a8afd7705ac002cc4dc7ec1ddaebe592837a1f73
nlemeur@fhcrc.org <nlemeur@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2007-04-05 00:20:19 +0000

    work on man page still warnings for summary methods and %&%
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@23624 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/filter-functions.R
+++ R/filter-functions.R
@@ -2,10 +2,11 @@
 ## function to print filter summary
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 print.filterSummary <- function(x,...) {
-	if(length(x$name) == 1) {
+    
+    if(length(x$name) == 1) {
 		with(x,cat(sprintf("%s: %d of %d (%.2f%%)\n",name,true,n,100*p)))
 	} else {
-		for(i in seq(along=x$name))
+	for(i in seq(along=x$name))
 			with(x,cat(sprintf("%s: %d of %d (%.2f%%)\n",name[i],true[i],n[i],100*p[i])))
 	}
 }
