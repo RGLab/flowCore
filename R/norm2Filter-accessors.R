b23ebc41ed28e082ad63df812f4236f57bda83d7
Deepayan Sarkar <dsarkar@fhcrc.org>
2007-04-30 19:05:45 +0000

    improved filtering using a norm2Filter, mostly by removing a couple of unnecessary exp() calls
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@24393 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/norm2Filter-accessors.R
+++ R/norm2Filter-accessors.R
@@ -18,8 +18,26 @@ setMethod("%in%",signature("flowFrame",table="norm2Filter"),function(x,table) {
 		stop("How did you get here?")
 		)
 	W  = t(y)-cov$center
-	result = exp(-.5*colSums((solve(cov$cov)%*%W)*W))>exp(-.5*table@scale.factor^2)
-	attr(result,'center') = cov$center
+
+        ## this used to be:
+
+        ## result = exp(-.5*colSums((solve(cov$cov)%*%W)*W))>exp(-.5*table@scale.factor^2)
+
+        ## which is bad because (1) it directly inverts cov$cov (which
+        ## is probably not too bad for a 2x2 matrix) and (2) uses
+        ## exp() unnecessarily (there are many rows, exp() is
+        ## expensive, but redundant as it is a monomotone increasing
+        ## transformation applied on both sides of the inequality).
+        ## -DS (2007/04/30)
+
+        result = colSums((qr.solve(cov$cov) %*% W) * W) < table@scale.factor^2
+
+        ## FIXME: a long term change might be to save chol(cov$cov)
+        ## rather than cov$cov in the result.  This helps in computing
+        ## the gate boundaries and qr.solve above could be replaced by
+        ## the equivalent of chol2inv(chol(cov$cov)).
+
+        attr(result,'center') = cov$center
 	attr(result,'cov')    = cov$cov
 	attr(result,'radius') = table@scale.factor
 	result
