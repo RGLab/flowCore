3d608581c1ee48c3a40a41dadfd3c0a9feba2e9f
fhahne <fhahne@bc3139a8-67e5-0310-9ffc-ced21a209358>
2008-07-23 21:27:24 +0000

    added a more programatic interface to create transformMaps
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@32874 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/filterSet-accessors.R
+++ R/filterSet-accessors.R
@@ -1,73 +1,78 @@
 setReplaceMethod("[[",
                  signature(x="filterSet", i="NULL", value="filter"),
-                 function(x, i, j, ..., value) {
-                     x@env[[paste(".",value@filterId, sep="")]] <- value
-                     x@env[[value@filterId]] <-
-                         filterReference(x, paste(".", value@filterId, sep=""))
-                     x
-                 })
+                 function(x, i, j, ..., value)
+             {
+                 n <- paste(".", value@filterId, sep="")
+                 x@env[[n]] <- value
+                 x@env[[value@filterId]] <- filterReference(x, n)
+                 x
+             })
 
 
 setReplaceMethod("[[",
                  signature(x="filterSet", i="ANY", value="filterReference"),
-                 function(x, i, j, ..., value) {
-                     ## Copy this filter in from another filterSet
-                     if(value@env != x@env)
-                         x[[i]] <- .resolveFilter(value)
-                     x
-                 })
+                 function(x, i, j, ..., value)
+             {
+                 ## Copy this filter in from another filterSet
+                 if(value@env != x@env)
+                     x[[i]] <- .resolveFilter(value)
+                 x
+             })
 
 setReplaceMethod("[[",
                  signature(x="filterSet", i="NULL", value="formula"),
-                 function(x, i, j, ..., value) {
-                     if(length(value) == 3) {
-                         a <- value[[2]]
-                         x[[as.character(a)]] <- value
-                     } else
-                     x[[""]] <- value
-                     x
-                 })
+                 function(x, i, j, ..., value)
+             {
+                 if(length(value) == 3) {
+                     a <- value[[2]]
+                     x[[as.character(a)]] <- value
+                 } else
+                 x[[""]] <- value
+                 x
+             })
 
 setReplaceMethod("[[",
                  signature(x="filterSet", i="character", value="formula"),
-                 function(x, i, j, ..., value) {
-                     ## Build a place to put names...
-                     e <- new.env(parent=x@env)
-                     harvest <- function(f) {
-                         if(is(f,"name") || is(f,"symbol") ||
-                            is(f,"character")) {
-                             f <- as.character(f)
-                             ## Create new virtual filters to be replaced
-                             ## later (or they will throw an error at some
-                             ## point).
-                             if(!exists(f,env=x@env)) {
-                                 e[[f]] <- filterReference(x,f)
-                             }
-                             filterReference(x,f)
-                         } else if(length(f) > 1 &&
-                                   as.character(f[[1]]) %in% c("!","&","|","%on%","%subset%","%&%")) {
-                             for(i in 2:length(f)) {
-                                 f[[i]] <- harvest(f[[i]])
-                             }
-                             f
-                         } else {
-                             f
+                 function(x, i, j, ..., value)
+             {
+                 ## Build a place to put names...
+                 e <- new.env(parent=x@env)
+                 harvest <- function(f) {
+                     if(is(f,"name") || is(f,"symbol") ||
+                        is(f,"character")) {
+                         f <- as.character(f)
+                         ## Create new virtual filters to be replaced
+                         ## later (or they will throw an error at some
+                         ## point).
+                         if(!exists(f,env=x@env)) {
+                             e[[f]] <- filterReference(x,f)
                          }
+                         filterReference(x,f)
+                     } else if(length(f) > 1 &&
+                               as.character(f[[1]]) %in% c("!","&","|","%on%",
+                                                           "%subset%","%&%")) {
+                         for(i in 2:length(f)) {
+                             f[[i]] <- harvest(f[[i]])
+                         }
+                         f
+                     } else {
+                         f
                      }
-	
-                     value <- harvest(value[[length(value)]])
-                     ## Extract the names from the formula to resolve to
-                     ## formula references
-                     old <- parent.env(x@env)
-                     parent.env(x@env) <- parent.frame()
-                     ## Evaluate in the environment with those names! This
-                     ## would be less work if RObjectTables had an R-level
-                     ## interface...
-                     y <- eval(value,e) 
-                     parent.env(x@env) <- old
-                     x[[i]] <- y
-                     x
-                 })
+                 }
+                 
+                 value <- harvest(value[[length(value)]])
+                 ## Extract the names from the formula to resolve to
+                 ## formula references
+                 old <- parent.env(x@env)
+                 parent.env(x@env) <- parent.frame()
+                 ## Evaluate in the environment with those names! This
+                 ## would be less work if RObjectTables had an R-level
+                 ## interface...
+                 y <- eval(value,e) 
+                 parent.env(x@env) <- old
+                 x[[i]] <- y
+                 x
+             })
 
 setReplaceMethod("[[",
                  signature(x="filterSet", i="missing", value="filter"),
@@ -75,12 +80,13 @@ setReplaceMethod("[[",
 
 setReplaceMethod("[[",
                  signature(x="filterSet", i="character", value="filter"),
-                 function(x,i,j,...,value) {
-                     if(nzchar(i))
-                         value@filterId <- i
-                     x[[NULL]] <- value
-                     x
-                 })
+                 function(x,i,j,...,value)
+             {
+                 if(nzchar(i))
+                     value@filterId <- i
+                 x[[NULL]] <- value
+                 x
+             })
 
 setMethod("[[",
           signature("filterSet", "character"),
@@ -92,22 +98,24 @@ setMethod("[[",
 ## Retrieve the filterReferences
 setMethod("[",
           signature("filterSet", "character"),
-          function(x, i, j, ..., drop=TRUE) {
-              if(length(i)==1)
-                  x@env[[i]]
-              else
-                  sapply(i,function(i) x@env[[i]])
-          })
+          function(x, i, j, ..., drop=TRUE)
+      {
+          if(length(i)==1)
+              x@env[[i]]
+          else
+              sapply(i,function(i) x@env[[i]])
+      })
 
 setMethod("show",
           signature("filterSet"),
-          function(object) {
-              cat("A set of filter objects:\n")
-              cat(paste(sapply(names(object), function(i)
-                               identifier(object@env[[i]])), sep=",",
-                        collapse=","))
-              cat("\n")
-          })
+          function(object)
+      {
+          cat("A set of filter objects:\n")
+          cat(paste(sapply(names(object), function(i)
+                           identifier(object@env[[i]])), sep=",",
+                    collapse=","))
+          cat("\n")
+      })
 
 setMethod("names",
           signature("filterSet"),
@@ -116,53 +124,53 @@ setMethod("names",
 ## Performs a topological sort of the filterSet (if possible)
 setMethod("sort",
           signature("filterSet"),
-          function(x, decreasing=FALSE, dependencies=FALSE, ...) {
-              n <- names(x)
-              ## Dependency matrix for this filter set
-              D <- diag(0,length(n))
-              row.names(D) <- n
-              colnames(D) <- n
-              for(i in n) {
-                  node <- x[[i]]
-                  if(is(node,"setOperationFilter")) {
-                      f <- node@filters
-                      ## You can "mask" an operation by building it directly
-                      ## into the formula. This supports Gating-ML semantics
-                      id <- identifier(node)
-                      n2 <- sapply(f,identifier)
-                      for(j in f[n2 %in% n]) {
-                          D[identifier(j),id] <- 1 
-                      }
+          function(x, decreasing=FALSE, dependencies=FALSE, ...)
+      {
+          n <- names(x)
+          ## Dependency matrix for this filter set
+          D <- diag(0,length(n))
+          row.names(D) <- n
+          colnames(D) <- n
+          for(i in n) {
+              node <- x[[i]]
+              if(is(node,"setOperationFilter")) {
+                  f <- node@filters
+                  ## You can "mask" an operation by building it directly
+                  ## into the formula. This supports Gating-ML semantics
+                  id <- identifier(node)
+                  n2 <- sapply(f,identifier)
+                  for(j in f[n2 %in% n]) {
+                      D[identifier(j),id] <- 1 
                   }
               }
-              ## Parent Set Size
-              P <- colSums(D)
-              O <- NULL 
+          }
+          ## Parent Set Size
+          P <- colSums(D)
+          O <- NULL 
+          G <- n[P>0]
+          while(length(G) > 0) {
+              O2 <- n[P==0]
+              l <- length(O2)
+              if(l > 0) {
+                  P[O2] <- -1
+                  P <- P - (if(l==1) D[O2,] else apply(D[O2,],2,sum))
+                  O <- c(O,O2)
+              }
               G <- n[P>0]
-              while(length(G) > 0) {
-                  O2 <- n[P==0]
-                  l <- length(O2)
-                  if(l > 0) {
-                      P[O2] <- -1
-                      P <- P - (if(l==1) D[O2,] else apply(D[O2,],2,sum))
-                      O <- c(O,O2)
-                  }
-                  G <- n[P>0]
-                  if(length(G)>0 && length(O2)==0)
-                      stop(paste("Circular reference detected in filterSet ",
-                                 "in one or more of:", paste(G, sep=",")))
-              }
-              ##Add in the leaves
-              O <- c(O,n[P==0])
-              if(decreasing)
-                  O <- rev(O)
-              if(dependencies)
-                  attr(O,"AdjM") <- D
-              O
-          })
+              if(length(G)>0 && length(O2)==0)
+                  stop(paste("Circular reference detected in filterSet ",
+                             "in one or more of:", paste(G, sep=",")))
+          }
+          ##Add in the leaves
+          O <- c(O,n[P==0])
+          if(decreasing)
+              O <- rev(O)
+          if(dependencies)
+              attr(O,"AdjM") <- D
+          O
+      })
 
 setMethod("filterReference",
           signature("filterSet", "character"),
-          function(from, name) {
-              new("filterReference", env=from@env,name=name)
-          })
+          function(from, name) new("filterReference", env=from@env, name=name)
+          )
--- R/transformList-accessors.R
+++ R/transformList-accessors.R
@@ -1,51 +1,77 @@
 ## ==========================================================================
-## colnames method
+## transformList allow us to programmatically access transformations.
+## They normaly contain items of class transformMap.
+## ==========================================================================
+
+## ==========================================================================
+## colnames method: This gives us the parameter names we want to transform
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-setMethod("colnames",signature("transformList"),function(x, do.NULL=TRUE, prefix="col") {
-	unique(sapply(x@transforms,slot,"input"))
-})
+setMethod("colnames",
+          signature("transformList"),
+          function(x, do.NULL=TRUE, prefix="col")
+      {
+          unique(sapply(x@transforms, slot, "input"))
+      })
+
 
 
 ## ==========================================================================
-## %on% operators
+## %on% operators: this is the real workhorse for doing %on% type
+## transformations. Eventually, all %on% methods are going to call this
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-setMethod("%on%",signature("transformList","flowFrame"),function(e1,e2) {
-	x <- exprs(e2)
-        pars <- parameters(e2)
-        ranges <- range(e2)
-	cN <- colnames(x)
-	for(y in e1@transforms){
-		if( !(y@output %in% cN) )
-			stop(y@output, "is not a variable in the flowFrame")
-		x[,y@output] = y@f(x[,y@input])
-                mt <- match(y@output, pars$name)
-                pData(pars)[mt,c("minRange", "maxRange")] <-
-                    y@f(ranges[,y@input])
-                
-	}
-	exprs(e2) <- x
-        parameters(e2) <- pars
-	e2
-})
-#General %on% implementation for a flowSet.
-setMethod("%on%",signature(e2="flowSet"),function(e1,e2) fsApply(e2,"%on%",e1=e1))
+setMethod("%on%",
+          signature("transformList", "flowFrame"),
+          function(e1, e2) {
+              x <- exprs(e2)
+              pars <- parameters(e2)
+              ranges <- range(e2)
+              cN <- colnames(x)
+              for(y in e1@transforms){
+                  if( !(y@output %in% cN) )
+                      stop(y@output, "is not a variable in the flowFrame")
+                  x[,y@output] = y@f(x[, y@input])
+                  mt <- match(y@output, pars$name)
+                  pData(pars)[mt,c("minRange", "maxRange")] <-
+                      y@f(ranges[,y@input])   
+              }
+              exprs(e2) <- x
+              parameters(e2) <- pars
+              e2
+          })
+
 
 
 ## ==========================================================================
-## summary method
-## So that we can get parameters back OUT of a transform
-## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-setMethod("summary",signature("transform"),function(object,..) {
-	e = environment(object)
-	x = ls(env=e)
-	structure(lapply(x,"get",env=e),names=x)
-})
+## coerce from a list to a transformLis
+setAs(from="list", to="transformList",
+      def=function(from) new("transformList", transforms=from))
 
-setMethod("c",signature("transformList"),function(x,...,recursive=FALSE) {
-	#Try to coerce everyone to a transformList first
-	all.t = lapply(list(...),as,"transformList")
-	params = c(sapply(x@transforms,slot,"output"),unlist(lapply(all.t,function(x) sapply(x@transforms,slot,"output"))))
-	if(length(params) > length(unique(params)))
-		stop("All output parameters must be unique when combining transforms")
-	new("transformList",transforms=c(x@transforms,unlist(sapply(all.t,slot,"transforms"))))
-})
+
+
+## ==========================================================================
+## concatenate two transformLists (or one list and a transformList)
+setMethod("c",
+          signature("transformList"),
+          function(x, ..., recursive=FALSE)
+      {
+          ## Try to coerce everyone to a transformList first
+          all.t <- lapply(list(...), as, "transformList")
+          params <- c(sapply(x@transforms, slot, "output"),
+                      unlist(lapply(all.t, function(x)
+                                    sapply(x@transforms, slot, "output"))))
+          if(length(params) > length(unique(params)))
+              stop("All output parameters must be unique when combining ",
+                   "transforms.", call.=FALSE)
+          new("transformList", transforms=c(x@transforms,
+                               unlist(sapply(all.t, slot, "transforms"))))
+      })
+
+
+
+## ==========================================================================
+## print details about the transformMaps in a transformList
+setMethod("show",
+          signature("transformMap"),
+          function(object) cat("transformMap for parameter '",
+                               object@input, "' mapping to '",
+                               object@output, "'\n", sep=""))
