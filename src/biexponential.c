c913a43fe4c802216942bd9925701d6d2c777c52
nlemeur@fhcrc.org <nlemeur@fhcrc.org@bc3139a8-67e5-0310-9ffc-ced21a209358>
2008-11-24 12:28:05 +0000

    fix biexponential function at zero where it should be zero and not NaN as noticed by Wade Rogers, U Penn;  Remove normalisation step of the spillover matrix in the compensation function which was an arbitrary process not justified according to Wade and Florian
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@35464 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/biexponential.c
+++ src/biexponential.c
@@ -12,6 +12,7 @@ double biexponential_fn(double x,void*info) {
 	int    sig;
 	if(x>p->g) { sig=1;x=x-p->g;} else {sig=0;x=p->g-x;}
 	double B = p->a*exp(p->b*x)-p->c*exp(-p->d*x)+p->f;
+	
 	return ((sig==1) ? B : -B)-p->y;
 }
 
@@ -38,13 +39,18 @@ SEXP biexponential_transform(SEXP input,SEXP A,SEXP B,SEXP C,SEXP D,SEXP F,SEXP 
 		double x;
 		int    MaxIt = asInteger(maxit);
 		params.y = REAL(output)[i];
-		x = params.y < 0 ? -log(-params.y)+params.g:log(params.y)-params.g;
-		//Find some starting parameters
-		for(j=0,Ax=.5;biexponential_fn(x-Ax,(void*)&params)>0;Ax*=10) if(++j > MaxIt) break;
-		for(j=0,Bx=.5;biexponential_fn(x+Bx,(void*)&params)<0;Bx*=10) if(++j > MaxIt) break;
-		REAL(output)[i] = R_zeroin(x-Ax,x+Bx,biexponential_fn,(void*)&params,&Tol,&MaxIt); 
-		if(MaxIt==0) 
-			fail++;
+		if(params.y!= 0) {
+		  x = params.y < 0 ? -log(-params.y)+params.g:log(params.y)-params.g;
+		  //Find some starting parameters
+		  for(j=0,Ax=.5;biexponential_fn(x-Ax,(void*)&params)>0;Ax*=10) if(++j > MaxIt) break;
+		  for(j=0,Bx=.5;biexponential_fn(x+Bx,(void*)&params)<0;Bx*=10) if(++j > MaxIt) break;
+		  REAL(output)[i] = R_zeroin(x-Ax,x+Bx,biexponential_fn,(void*)&params,&Tol,&MaxIt); 
+		  if(MaxIt==0) 
+		    fail++;
+		}
+		else{
+		  REAL(output)[i] = 0;
+		}
 	}
 	if(fail>0)
 		warning("%d values of %d have not converged.",fail,length(output));
