b8463bf2a877a38551b2f2e187f5ba74bb88627e
Nishant Gopalakrishnan <ngopalak@fhcrc.org>
2008-12-13 01:28:28 +0000

    Added registration for C functions
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@35918 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ src/flowCore.h
@@ -0,0 +1,10 @@
+#include <R.h>
+#include <Rinternals.h>
+
+SEXP biexponential_transform(SEXP input,SEXP A,SEXP B,SEXP C,SEXP D,SEXP F,SEXP G,SEXP tol,SEXP maxit);
+SEXP inPolygon(SEXP _data, SEXP _vertices);
+SEXP inPolytope(SEXP _data, SEXP _A, SEXP _b);
+
+//  SEXP biexponential_transform(SEXP *,SEXP *,SEXP *,SEXP *,SEXP *,SEXP *,SEXP *,SEXP *,SEXP *);
+//  SEXP inPolygon(SEXP *, SEXP *);
+//  SEXP inPolytope(SEXP *, SEXP *, SEXP *);
