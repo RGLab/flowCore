c93471d698b5e1cbd8739135c2b3d2a2c4193aec
n.gopalakrishnan <n.gopalakrishnan@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-04-14 23:17:49 +0000

    Updated biexponential transform so that it now calculates the solution of a generic biexponential function S(x,a,b,c,d,f) = ae^(bx) -ce^(-dx) +f  instead of the logicle function it was calculating earlier.
    Updated Logicle transformation: The implementation of the logicle
    transform
    was found to be different from that described by Parks et al. A new C
    function 
    was implemented for the logicle transformation reflecting the method
    described 
    in the paper. Additionally, the parameter argument f passed to  the
    logicle 
    function was missing a multiplying factor a.
    
    
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@38805 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/biexponential.c
+++ src/biexponential.c
@@ -2,20 +2,17 @@
 #include <R.h>
 #include <R_ext/Applic.h>
 
-
 struct biexponential_info {
-	double a,b,c,d,f,g,y;
+	double a,b,c,d,f,w,y;
 };
 
 double biexponential_fn(double x,void*info) {
 	struct biexponential_info *p = (struct biexponential_info *)info;
-	int    sig;
-	if(x>p->g) { sig=1;x=x-p->g;} else {sig=0;x=p->g-x;}
-	double B = p->a*exp(p->b*x)-p->c*exp(-p->d*x)+p->f;
-	
-	return ((sig==1) ? B : -B)-p->y;
+	double B = p->a*exp(p->b*(x-p->w))-p->c*exp(-p->d*(x-p->w))+p->f -p-> y;
+	//return ((x>p->w) ? B : -B);
+        return (B);
 }
-
+/*
 
 SEXP biexponential_transform(SEXP input,SEXP A,SEXP B,SEXP C,SEXP D,SEXP F,SEXP G,SEXP tol,SEXP maxit) {
 	SEXP    output;
@@ -52,6 +49,96 @@ SEXP biexponential_transform(SEXP input,SEXP A,SEXP B,SEXP C,SEXP D,SEXP F,SEXP 
 		  REAL(output)[i] = 0;
 		}
 	}
+	if(fail>0)
+		warning("%d values of %d have not converged.",fail,length(output));
+	UNPROTECT(1);
+	return output;
+}*/
+
+/*4/14/09 ngopalak: Updated biexponential transform so that it now calculates
+the solution of a generic biexponential function 
+S(x,a,b,c,d,f) = ae^(bx) -ce^(-dx) +f  instead of the logicle function
+it was calculating earlier
+*/
+SEXP biexponential_transform(SEXP input,SEXP A,SEXP B,SEXP C,SEXP D,SEXP F,SEXP W,SEXP tol,SEXP maxit) {
+	SEXP    output;
+	struct biexponential_info params;
+	
+	int    i;
+        int fail=0;
+	double step,checkme=1;
+	params.a = asReal(A);
+	params.b = asReal(B);
+	params.c = asReal(C);
+	params.d = asReal(D);
+	params.f = asReal(F);
+        params.w = asReal(W);
+
+	if(TYPEOF(input) != REALSXP) error("Input must be real values.");
+	PROTECT(output = duplicate(input));
+	for(i=0;i<length(output);i++) {
+		int    j;
+		double Tol   = asReal(tol);
+		double x,y;
+		int    MaxIt = asInteger(maxit);
+		double res;
+		params.y = REAL(output)[i];
+                    for(j=0,step=0.5; biexponential_fn(-step,(void*)&params)*biexponential_fn(step,(void*)&params) >0;step*=1.5,j+=1){  
+                        if(j > MaxIt){
+                              break;
+                          }
+                    }
+                    REAL(output)[i]=R_zeroin(-step,step,biexponential_fn,(void*)&params,&Tol,&MaxIt); 
+                    if(MaxIt==-1){ 
+                        fail=fail+1;
+                    }
+        }
+	if(fail>0)
+		warning("%d values of %d have not converged.",fail,length(output));
+	UNPROTECT(1);
+	return output;
+}
+
+/*4/14/09 ngopalak: Updated Logicle transformation. the logicle transformation
+ was implemented incorrectly.Additionally, the parameter argument f passed to 
+this function was missing a multiplying
+factor a
+*/
+
+SEXP logicle_transform(SEXP input,SEXP A,SEXP B,SEXP C,SEXP D,SEXP F,SEXP W,SEXP tol,SEXP maxit) {
+	SEXP    output;
+	struct biexponential_info params;
+	int     i,fail=0;
+	double step,checkme=1;
+	params.a = asReal(A);
+	params.b = asReal(B);
+	params.c = asReal(C);
+	params.d = asReal(D);
+	params.f = asReal(F);
+	params.w = asReal(W);
+	
+	if(TYPEOF(input) != REALSXP) error("Input must be real values.");
+	PROTECT(output = duplicate(input));
+	for(i=0;i<length(output);i++) {
+		int    j;
+		double Tol   = asReal(tol);
+		double x,y;
+		int    MaxIt = asInteger(maxit);
+		double res;
+		y = REAL(output)[i];
+		params.y = (y >= params.w) ? y: 2*params.w-y;
+		
+                for(j=0,step=0.5; biexponential_fn(-step,(void*)&params)*biexponential_fn(step,(void*)&params) >0;step*=1.5,j+=1){  
+                    if(j > MaxIt){
+                        break;
+                    }
+                }
+                res=R_zeroin(-step,step,biexponential_fn,(void*)&params,&Tol,&MaxIt); 
+                REAL(output)[i] = (y>= params.w) ? res : -res;
+                if(MaxIt==-1){ 
+                        fail=fail+1;
+                }
+        }
 	if(fail>0)
 		warning("%d values of %d have not converged.",fail,length(output));
 	UNPROTECT(1);
