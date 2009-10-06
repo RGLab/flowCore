302017d59401618e9ef2a0432ccad4ceff86a79c
n.gopalakrishnan <n.gopalakrishnan@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-10-06 22:55:34 +0000

    Added C functions for calculating the inverse of the logicleTransform using the general biexponential function
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@42204 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/biexponential.c
+++ src/biexponential.c
@@ -12,48 +12,6 @@ double biexponential_fn(double x,void*info) {
 	//return ((x>p->w) ? B : -B);
         return (B);
 }
-/*
-
-SEXP biexponential_transform(SEXP input,SEXP A,SEXP B,SEXP C,SEXP D,SEXP F,SEXP G,SEXP tol,SEXP maxit) {
-	SEXP    output;
-	struct biexponential_info params;
-	
-	int     i,fail=0;
-	
-	params.a = asReal(A);
-	params.b = asReal(B);
-	params.c = asReal(C);
-	params.d = asReal(D);
-	params.f = asReal(F);
-	params.g = asReal(G);
-	
-	if(TYPEOF(input) != REALSXP) error("Input must be real values.");
-	PROTECT(output = duplicate(input));
-	for(i=0;i<length(output);i++) {
-		int    j;
-		double Ax,Bx;
-		double Tol   = asReal(tol);
-		double x;
-		int    MaxIt = asInteger(maxit);
-		params.y = REAL(output)[i];
-		if(params.y!= 0) {
-		  x = params.y < 0 ? -log(-params.y)+params.g:log(params.y)-params.g;
-		  //Find some starting parameters
-		  for(j=0,Ax=.5;biexponential_fn(x-Ax,(void*)&params)>0;Ax*=10) if(++j > MaxIt) break;
-		  for(j=0,Bx=.5;biexponential_fn(x+Bx,(void*)&params)<0;Bx*=10) if(++j > MaxIt) break;
-		  REAL(output)[i] = R_zeroin(x-Ax,x+Bx,biexponential_fn,(void*)&params,&Tol,&MaxIt); 
-		  if(MaxIt==0) 
-		    fail++;
-		}
-		else{
-		  REAL(output)[i] = 0;
-		}
-	}
-	if(fail>0)
-		warning("%d values of %d have not converged.",fail,length(output));
-	UNPROTECT(1);
-	return output;
-}*/
 
 /*4/14/09 ngopalak: Updated biexponential transform so that it now calculates
 the solution of a generic biexponential function 
@@ -141,6 +99,42 @@ SEXP logicle_transform(SEXP input,SEXP A,SEXP B,SEXP C,SEXP D,SEXP F,SEXP W,SEXP
         }
 	if(fail>0)
 		warning("%d values of %d have not converged.",fail,length(output));
+	UNPROTECT(1);
+	return output;
+}
+
+/*10/06/09 ngopalak: Added a function to calculate the inverse of the logicle 
+transformation, making use of the previously defined biexponential_fn to calculate 
+the inverse
+*/
+
+SEXP invLogicle_transform(SEXP input,SEXP A,SEXP B,SEXP C,SEXP D,SEXP F,SEXP W){
+	SEXP    output;
+	struct biexponential_info params;
+	int     i;
+	params.a = asReal(A);
+	params.b = asReal(B);
+	params.c = asReal(C);
+	params.d = asReal(D);
+	params.f = asReal(F);
+	params.w = asReal(W);
+	params.y = 0 ;
+	if(TYPEOF(input) != REALSXP) error("Input must be real values.");
+	PROTECT(output = duplicate(input));
+	for(i=0;i<length(output);i++) {
+		double res;
+		double x;
+		x = REAL(output)[i];
+		if( x >= 0){
+			res = biexponential_fn(x,(void*)&params);
+		
+		}else{
+			x = 2*params.w -x;
+		    res = -biexponential_fn(x,(void*)&params);
+		}
+		REAL(output)[i] = res;
+	
+	}
 	UNPROTECT(1);
 	return output;
 }
--- src/flowCore.h
+++ src/flowCore.h
@@ -3,6 +3,7 @@
 
 SEXP biexponential_transform(SEXP input,SEXP A,SEXP B,SEXP C,SEXP D,SEXP F,SEXP G,SEXP tol,SEXP maxit);
 SEXP logicle_transform(SEXP input,SEXP A,SEXP B,SEXP C,SEXP D,SEXP F,SEXP G,SEXP tol,SEXP maxit);
+SEXP invLogicle_transform(SEXP input,SEXP A,SEXP B,SEXP C,SEXP D,SEXP F,SEXP G);
 SEXP inPolygon(SEXP _data, SEXP _vertices);
 SEXP inPolytope(SEXP _data, SEXP _A, SEXP _b);
 
