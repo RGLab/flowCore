dee33c00815b8106e818f30a7b958919fa880578
n.gopalakrishnan <n.gopalakrishnan@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-10-20 08:36:34 +0000

    updated the Logicle transformation to use a separate C function for its calculation rather than a generic biexponential funcion
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@42504 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/biexponential.c
+++ src/biexponential.c
@@ -8,8 +8,23 @@ struct biexponential_info {
 
 double biexponential_fn(double x,void*info) {
 	struct biexponential_info *p = (struct biexponential_info *)info;
-	double B = p->a*pow(10,p->b*(x-p->w))-p->c*pow(10,-p->d*(x-p->w))+p->f -p-> y;
+	double B = p->a*exp(p->b*(x-p->w))-p->c*exp(-p->d*(x-p->w))+p->f -p-> y;
     return (B);
+}
+
+struct sfun_info{
+	double m,w,p,t,a,r;
+};
+
+/*
+Logicle function calculation 
+*/
+double strans_fn(double y,void*info){
+	struct sfun_info  *k = (struct sfun_info *)info;
+	double tmp ;
+	tmp = (y < (k->w +k->a)) ? (k->w + k->a - y) : (y - k->w - k->a) ; 
+	tmp =  k->t*pow(10,-1*(k->m - k->w - k->a ))*(pow(10,tmp)-k->p*k->p*pow(10,-tmp/k->p)+ k->p*k->p-1);	
+	return((y < (k->w +k->a)) ? -1*tmp -k->r  :tmp-k->r);
 }
 
 /*10/15/09 ngopalak: Updated biexponential transform so that it now calculates
@@ -29,7 +44,7 @@ SEXP biexponential_transform(SEXP input,SEXP A,SEXP B,SEXP C,SEXP D,SEXP F,SEXP 
 	params.c = asReal(C);
 	params.d = asReal(D);
 	params.f = asReal(F);
-        params.w = asReal(W);
+    params.w = asReal(W);
 
 	if(TYPEOF(input) != REALSXP) error("Input must be real values.");
 	PROTECT(output = duplicate(input));
@@ -54,54 +69,44 @@ SEXP biexponential_transform(SEXP input,SEXP A,SEXP B,SEXP C,SEXP D,SEXP F,SEXP 
 	return output;
 }
 
-/*10/15/09 ngopalak: Updated Logicle transformation to decade units to be 
-compliant with the definition of transform defined by gatingML standards. 
-*/
+/*
+10/20/09 ngopalak: Updated the logicle transform so that it now uses a separate logicle function strans_info
+instead of the generic biexponential function being used earlier. This change was necessary to 
+simplify the implementation and improve code readability.
 
-SEXP logicle_transform(SEXP input,SEXP A,SEXP B,SEXP C,SEXP D,SEXP F,SEXP W,SEXP tol,SEXP maxit) {
+*/
+SEXP logicle_transform(SEXP input,SEXP M,SEXP W,SEXP P,SEXP T,SEXP A,SEXP tol,SEXP maxit) {
 	SEXP    output;
-	struct biexponential_info params;
+	struct sfun_info params;
 	int     i,fail=0;
 	double step;
-	params.a = asReal(A);
-	params.b = asReal(B);
-	params.c = asReal(C);
-	params.d = asReal(D);
-	params.f = asReal(F);
+	params.m = asReal(M);
 	params.w = asReal(W);
-	
+	params.p = asReal(P);
+	params.t = asReal(T);
+	params.a = asReal(A);
+	double Tol   = asReal(tol);
+	int    MaxIt ;	
 	if(TYPEOF(input) != REALSXP) error("Input must be real values.");
 	PROTECT(output = duplicate(input));
 	for(i=0;i<length(output);i++) {
 		int    j;
-		double Tol   = asReal(tol);
-		double y;
-		int    MaxIt = asInteger(maxit);
 		double res;
-		y = REAL(output)[i];
-		params.y = y;
-		for(j=0,step=0.5; biexponential_fn(-step,(void*)&params)*biexponential_fn(step,(void*)&params) >0;step*=1.5,j+=1){  
+		double y = REAL(output)[i];
+		MaxIt = asInteger(maxit);
+      	params.r =y;
+ 		for(j=0,step=0.5; strans_fn(-step,(void*)&params)*strans_fn(step,(void*)&params) >0;step*=1.5,j+=1){  
             if(j > MaxIt){
                 break;
             }
         }
-        res=R_zeroin(-step,step,biexponential_fn,(void*)&params,&Tol,&MaxIt); 
-		if(res < params.w){
-			y = 2*params.w - y;
-			params.y = y;
-		    for(j=0,step=0.5; biexponential_fn(-step,(void*)&params)*biexponential_fn(step,(void*)&params) >0;step*=1.5,j+=1){  
-				if(j > MaxIt){
-					break;
-				}
-			}
-			res= -1*R_zeroin(-step,step,biexponential_fn,(void*)&params,&Tol,&MaxIt); 
-	
-        }
-        REAL(output)[i] = res;
+        res=R_zeroin(-step,step,strans_fn,(void*)&params,&Tol,&MaxIt); 
+	    REAL(output)[i] = res;
         if(MaxIt==-1){ 
             fail=fail+1;
-            }
         }
+	
+    }
 	if(fail>0)
 		warning("%d values of %d have not converged.",fail,length(output));
 	UNPROTECT(1);
@@ -113,23 +118,22 @@ transformation, making use of the previously defined biexponential_fn to calcula
 the inverse
 */
 
-SEXP invLogicle_transform(SEXP input,SEXP A,SEXP B,SEXP C,SEXP D,SEXP F,SEXP W){
+SEXP invLogicle_transform(SEXP input,SEXP M,SEXP W,SEXP P,SEXP T,SEXP A){
 	SEXP    output;
-	struct biexponential_info params;
+	struct sfun_info params;
 	int     i;
-	params.a = asReal(A);
-	params.b = asReal(B);
-	params.c = asReal(C);
-	params.d = asReal(D);
-	params.f = asReal(F);
+	params.m = asReal(M);
 	params.w = asReal(W);
-	params.y = 0 ;
+	params.p = asReal(P);
+	params.t = asReal(T);
+	params.a = asReal(A);
+	params.r = 0 ;
 	if(TYPEOF(input) != REALSXP) error("Input must be real values.");
 	PROTECT(output = duplicate(input));
 	for(i=0;i<length(output);i++) {
 		double x;
 		x = REAL(output)[i];
-		REAL(output)[i] =  (x >= 0) ? biexponential_fn(x,(void*)&params): 2*params.w-biexponential_fn(-x,(void*)&params);
+		REAL(output)[i] =  strans_fn(x,(void*)&params);
 	}
 	UNPROTECT(1);
 	return output;
