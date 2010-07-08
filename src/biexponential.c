20621d3b435bf06fcd4258abe81b57ad8aed975d
n.gopalakrishnan <n.gopalakrishnan@bc3139a8-67e5-0310-9ffc-ced21a209358>
2010-07-08 00:12:32 +0000

    Added the Logicle class provided by Wayne to the package. Added wrappers to make use of the transformation functions provided by the Logicle class
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@47981 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/biexponential.c
+++ src/biexponential.c
@@ -18,7 +18,7 @@ struct sfun_info{
 
 /*
 Logicle function calculation 
-*/
+
 double strans_fn(double y,void*info){
 	struct sfun_info  *k = (struct sfun_info *)info;
 	double tmp ;
@@ -26,7 +26,7 @@ double strans_fn(double y,void*info){
 	tmp =  k->t*pow(10,-1*(k->m - k->w - k->a ))*(pow(10,tmp)-k->p*k->p*pow(10,-tmp/k->p)+ k->p*k->p-1);	
 	return((y < (k->w +k->a)) ? -1*tmp -k->r  :tmp-k->r);
 }
-
+*/
 /*10/15/09 ngopalak: Updated biexponential transform so that it now calculates
 the solution of a generic biexponential function 
 S(x,a,b,c,d,f) = ae^(bx) -ce^(-dx) +f  instead of the logicle function
@@ -69,72 +69,3 @@ SEXP biexponential_transform(SEXP input,SEXP A,SEXP B,SEXP C,SEXP D,SEXP F,SEXP 
 	return output;
 }
 
-/*
-10/20/09 ngopalak: Updated the logicle transform so that it now uses a separate logicle function strans_info
-instead of the generic biexponential function being used earlier. This change was necessary to 
-simplify the implementation and improve code readability.
-
-*/
-SEXP logicle_transform(SEXP input,SEXP M,SEXP W,SEXP P,SEXP T,SEXP A,SEXP tol,SEXP maxit) {
-	SEXP    output;
-	struct sfun_info params;
-	int     i,fail=0;
-	double step;
-	params.m = asReal(M);
-	params.w = asReal(W);
-	params.p = asReal(P);
-	params.t = asReal(T);
-	params.a = asReal(A);
-	double Tol   = asReal(tol);
-	int    MaxIt ;	
-	if(TYPEOF(input) != REALSXP) error("Input must be real values.");
-	PROTECT(output = duplicate(input));
-	for(i=0;i<length(output);i++) {
-		int    j;
-		double res;
-		double y = REAL(output)[i];
-		MaxIt = asInteger(maxit);
-      	params.r =y;
- 		for(j=0,step=0.5; strans_fn(-step,(void*)&params)*strans_fn(step,(void*)&params) >0;step*=1.5,j+=1){  
-            if(j > MaxIt){
-                break;
-            }
-        }
-        res=R_zeroin(-step,step,strans_fn,(void*)&params,&Tol,&MaxIt); 
-	    REAL(output)[i] = res;
-        if(MaxIt==-1){ 
-            fail=fail+1;
-        }
-	
-    }
-	if(fail>0)
-		warning("%d values of %d have not converged.",fail,length(output));
-	UNPROTECT(1);
-	return output;
-}
-
-/*10/06/09 ngopalak: Added a function to calculate the inverse of the logicle 
-transformation, making use of the previously defined biexponential_fn to calculate 
-the inverse
-*/
-
-SEXP invLogicle_transform(SEXP input,SEXP M,SEXP W,SEXP P,SEXP T,SEXP A){
-	SEXP    output;
-	struct sfun_info params;
-	int     i;
-	params.m = asReal(M);
-	params.w = asReal(W);
-	params.p = asReal(P);
-	params.t = asReal(T);
-	params.a = asReal(A);
-	params.r = 0 ;
-	if(TYPEOF(input) != REALSXP) error("Input must be real values.");
-	PROTECT(output = duplicate(input));
-	for(i=0;i<length(output);i++) {
-		double x;
-		x = REAL(output)[i];
-		REAL(output)[i] =  strans_fn(x,(void*)&params);
-	}
-	UNPROTECT(1);
-	return output;
-}
