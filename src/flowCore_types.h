60b66830f6f7d348ce6574d3f46b603cc2525c3a
Mike Jiang <wjiang2@fhcrc.org>
2015-11-24 21:14:49 +0000

    add flowCore_types.h file so that RcppExport.cpp can automatically take care of the custom header include.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@110886 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ src/flowCore_types.h
@@ -0,0 +1,16 @@
+/*
+ * flowCore_types.h
+ *
+ *  Created on: Nov 24, 2015
+ *      Author: wjiang2
+ */
+
+#ifndef FLOWCORE_TYPES_H_
+#define FLOWCORE_TYPES_H_
+
+
+#include "pairVectorRcppWrap.h"
+#include "convertRawBytes.h"
+
+
+#endif /* FLOWCORE_TYPES_H_ */
