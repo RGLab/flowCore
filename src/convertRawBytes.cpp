70ceef7852c1e060c391c75f5b44db885e2d2a4f
mikejiang <wjiang2@fhcrc.org>
2015-11-30 13:13:18 -0800

    take care of conversions for different size of integer and float. #46

--- src/convertRawBytes.cpp
+++ src/convertRawBytes.cpp
@@ -1,6 +1,31 @@
 #include <Rcpp.h>
 #include "convertRawBytes.h"
 
+template <class T>
+T convertRaw_impl(const BYTES & src, unsigned short thisSize, bool isBigEndian, unsigned thisStart, unsigned thisEnd)
+{
+  T dest;
+  if(isBigEndian)
+  {
+    BYTES tmp(thisSize);
+    
+    for(auto k = thisStart; k <= thisEnd; k++)
+      tmp.at(k % thisSize) = src.at(k);
+    std::reverse(tmp.begin(),tmp.end());
+    
+    //         for(auto byte : tmp)
+    //           Rcpp::Rcout << std::hex << int(byte);
+    //         Rcpp::Rcout << std::endl;
+    
+    memcpy(&dest, &tmp.at(0), thisSize);
+  }
+  else
+  {
+    memcpy(&dest, &src.at(thisStart), thisSize);
+  }
+  
+  return dest;
+}
 
 
 // [[Rcpp::plugins("cpp11")]]
@@ -9,12 +34,26 @@
  * convert the raw vector to the respective type with the data that has different colSize across parameters
  * The input is from readBin call
  */
-template <class T>
-void convertRawBytes_impl( BYTES bytes,  std::vector<T> & output, std::vector<unsigned short> colSize
-                    , unsigned short ncol, unsigned nrow, bool isBigEndian) 
-  {
+// [[Rcpp::export]]
+std::vector<double> convertRawBytes( BYTES bytes, bool isInt, std::vector<unsigned short> colSize
+                    , unsigned short ncol, bool isBigEndian) 
+{
+
+  if(colSize.size()!=ncol)
+    Rcpp::stop("The length of 'colSize' vector does not match to 'ncol'!");
   
-  
+  unsigned nBytes = bytes.size();
+  //total bytes for each row
+  unsigned short nRowSize = 0;
+  for(auto size : colSize)
+    nRowSize+=size;
+  //how many rows
+  unsigned nrow = nBytes/nRowSize;
+  //how many element to return
+  auto nElement = nrow * ncol;
+  //for the simplicity, we always convert the data to double regardless of datatype
+  //because we need to do this anyway when datatype uint32 
+  std::vector<double> output(nElement);
   
   auto pos = 0;
   // Rcpp::Rcout << "event=" << nrow << " ncol=" << ncol << std::endl;
@@ -38,82 +77,42 @@ void convertRawBytes_impl( BYTES bytes,  std::vector<T> & output, std::vector<un
 //       for(auto k = thisStart; k <= thisEnd; k++)
 //         Rcpp::Rcout << std::hex << int(bytes.at(k));
 //       Rcpp::Rcout << std::endl;
+      if(isInt){
+        switch(thisSize)
+        {
+        case sizeof(BYTE)://1 byte
+            output.at(ind) = (double)convertRaw_impl<BYTE>(bytes, thisSize, isBigEndian, thisStart, thisEnd);
+            break;
+        case sizeof(unsigned short): //2 bytes
+          output.at(ind) = (double)convertRaw_impl<unsigned short>(bytes, thisSize, isBigEndian, thisStart, thisEnd);
+          break;
+        case sizeof(unsigned)://4 bytes
+          output.at(ind) = (double)convertRaw_impl<unsigned>(bytes, thisSize, isBigEndian, thisStart, thisEnd);
+          break;
+        default:
+          Rcpp::stop("Unsupported bitwidths when performing channel-wise reading:", thisSize);
+        }
         
-      if(isBigEndian)
-      {
-        BYTES tmp(thisSize);
-        
-        for(auto k = thisStart; k <= thisEnd; k++)
-            tmp.at(k % thisSize) = bytes.at(k);
-        std::reverse(tmp.begin(),tmp.end());
-        
-//         for(auto byte : tmp)
-//           Rcpp::Rcout << std::hex << int(byte);
-//         Rcpp::Rcout << std::endl;
-        
-        memcpy(&output.at(ind), &tmp.at(0), thisSize);
       }
       else
       {
-        memcpy(&output.at(ind), &bytes.at(thisStart), thisSize);
+        switch(thisSize)
+        {
+        case sizeof(float):
+          output.at(ind) = (double)convertRaw_impl<float>(bytes, thisSize, isBigEndian, thisStart, thisEnd);
+          break;
+        case sizeof(double):
+          output.at(ind) = (double)convertRaw_impl<double>(bytes, thisSize, isBigEndian, thisStart, thisEnd);
+          break;
+        default:
+          Rcpp::stop("Unsupported bitwidths when performing channel-wise reading:", thisSize);
+        }
       }
+      
         
       pos = thisEnd + 1;	          
     } 
   }
-  
+  return output;
 }
 
-
-
-
-// [[Rcpp::export]]
-SEXP convertRawBytes(BYTES bytes, bool isInt, std::vector<unsigned short> colSize
-                , unsigned short ncol, bool isBigEndian){
-  
-  // RCPP_RETURN_VECTOR does not work here since it only takes one function argument
-  if(colSize.size()!=ncol)
-    Rcpp::stop("The length of 'colSize' vector does not match to 'ncol'!");
-  
-  unsigned nBytes = bytes.size();
-  //total bytes for each row
-  unsigned short nRowSize = 0;
-  for(auto size : colSize)
-    nRowSize+=size;
-  //how many rows
-  unsigned nrow = nBytes/nRowSize;
-  //how many element to return
-  auto nElement = nrow * ncol;
-  
-  
-  if(isInt)
-  {
-    std::vector<int> output(nElement);
-    convertRawBytes_impl<int>(bytes, output, colSize, ncol,nrow, isBigEndian) ;
-    return Rcpp::wrap(output);
-  }
-  else
-  {
-    //we only support the same byte size for numeric type
-    switch(colSize.at(0))
-    {
-    case sizeof(float):
-        {
-            std::vector<float> output(nElement);
-            convertRawBytes_impl<float>(bytes, output, colSize, ncol,nrow, isBigEndian);
-            return Rcpp::wrap(output);
-        }   
-    case sizeof(double):
-        {
-          std::vector<double> output(nElement);
-          convertRawBytes_impl<double>(bytes, output, colSize, ncol, nrow,isBigEndian);    
-          return Rcpp::wrap(output);
-        }
-      
-    }
-    
-  }
-    
-  
-  
-}
