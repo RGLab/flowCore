6ace476f48f1fdca8ff29ebeb07da90fdb4b804d
Mike Jiang <wjiang2@fhcrc.org>
2016-01-15 22:16:01 +0000

    remove the deprecated 'register' Storage class specifiers in legacy cpp code.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@112614 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/Logicle.cpp
+++ src/Logicle.cpp
@@ -233,7 +233,7 @@ double Logicle::R_zeroin2(			/* An estimate of the root */
 	if( fabs(prev_step) >= tol_act	/* If prev_step was large enough*/
 	    && fabs(fa) > fabs(fb) ) {	/* and was in true direction,
 					 * Interpolation may be tried	*/
-	    register double t1,cb,t2;
+	    double t1,cb,t2;
 	    cb = c-b;
 	    if( a==c ) {		/* If we have only two distinct	*/
 					/* points linear interpolation	*/
