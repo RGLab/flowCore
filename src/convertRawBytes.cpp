9daa4223f78b341bce430db5b6dc843fdc6967d1
mikejiang <wjiang2@fhcrc.org>
2016-05-04 15:08:46 -0700

    fix the bug in throwing exception message. #59

--- src/convertRawBytes.cpp
+++ src/convertRawBytes.cpp
@@ -8,22 +8,22 @@ T convertRaw_impl(const BYTES & src, unsigned short thisSize, bool isBigEndian, 
   if(isBigEndian)
   {
     BYTES tmp(thisSize);
-    
+
     for(auto k = thisStart; k <= thisEnd; k++)
       tmp.at(k % thisSize) = src.at(k);
     std::reverse(tmp.begin(),tmp.end());
-    
+
     //         for(auto byte : tmp)
     //           Rcpp::Rcout << std::hex << int(byte);
     //         Rcpp::Rcout << std::endl;
-    
+
     memcpy(&dest, &tmp.at(0), thisSize);
   }
   else
   {
     memcpy(&dest, &src.at(thisStart), thisSize);
   }
-  
+
   return dest;
 }
 
@@ -36,12 +36,12 @@ T convertRaw_impl(const BYTES & src, unsigned short thisSize, bool isBigEndian, 
  */
 // [[Rcpp::export]]
 std::vector<double> convertRawBytes( BYTES bytes, bool isInt, std::vector<unsigned short> colSize
-                    , unsigned short ncol, bool isBigEndian) 
+                    , unsigned short ncol, bool isBigEndian)
 {
 
   if(colSize.size()!=ncol)
     Rcpp::stop("The length of 'colSize' vector does not match to 'ncol'!");
-  
+
   unsigned nBytes = bytes.size();
   //total bytes for each row
   unsigned short nRowSize = 0;
@@ -52,31 +52,32 @@ std::vector<double> convertRawBytes( BYTES bytes, bool isInt, std::vector<unsign
   //how many element to return
   auto nElement = nrow * ncol;
   //for the simplicity, we always convert the data to double regardless of datatype
-  //because we need to do this anyway when datatype uint32 
+  //because we need to do this anyway when datatype uint32
   std::vector<double> output(nElement);
-  
+
   auto pos = 0;
   // Rcpp::Rcout << "event=" << nrow << " ncol=" << ncol << std::endl;
-  
+
   for(auto i = 0; i < nrow; i++){
 
     for(auto j = 0; j < ncol; j++){
       //convert each element
       auto thisStart = pos;
       auto thisSize = colSize.at(j);
-      
+
       if(thisSize != 1 && thisSize != 2 && thisSize !=  4 && thisSize !=  8)
         Rcpp::stop("Multiple different odd bitwidths are not supported!");
-      
+
       auto thisEnd = pos + thisSize - 1;
       auto ind = i * ncol + j;
-      
+
       // Rcpp::Rcout << "start="  << thisStart << " " << "end=" << thisEnd << " " << "index=" << ind << std::endl;
       // Rcpp::Rcout << "bytes" << std::endl;
-      
+
 //       for(auto k = thisStart; k <= thisEnd; k++)
 //         Rcpp::Rcout << std::hex << int(bytes.at(k));
 //       Rcpp::Rcout << std::endl;
+      // Rcpp::Rcout << std::to_string(thisSize) << std::endl;
       if(isInt){
         switch(thisSize)
         {
@@ -90,9 +91,9 @@ std::vector<double> convertRawBytes( BYTES bytes, bool isInt, std::vector<unsign
           output.at(ind) = (double)convertRaw_impl<unsigned>(bytes, thisSize, isBigEndian, thisStart, thisEnd);
           break;
         default:
-          Rcpp::stop("Unsupported bitwidths when performing channel-wise reading:", thisSize);
+          Rcpp::stop("Unsupported bitwidths when performing channel-wise reading:" + std::to_string(thisSize));
         }
-        
+
       }
       else
       {
@@ -105,13 +106,13 @@ std::vector<double> convertRawBytes( BYTES bytes, bool isInt, std::vector<unsign
           output.at(ind) = (double)convertRaw_impl<double>(bytes, thisSize, isBigEndian, thisStart, thisEnd);
           break;
         default:
-          Rcpp::stop("Unsupported bitwidths when performing channel-wise reading:", thisSize);
+          Rcpp::stop("Unsupported bitwidths when performing channel-wise reading:" + std::to_string(thisSize));
         }
       }
-      
-        
-      pos = thisEnd + 1;	          
-    } 
+
+
+      pos = thisEnd + 1;
+    }
   }
   return output;
 }
