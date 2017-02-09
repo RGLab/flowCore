1d77658e0d45920535488ef88fd546a9fd02719e
Mike Jiang <wjiang2@fhcrc.org>
2017-02-09 23:42:42 +0000

    add unit test for multi-data segment. #71
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/flowCore@126651 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,4 +6,4 @@ test_check("flowCore")
 
 #devtools::test("~/rglab/workspace/flowCore")
 #the FCS files are not included in package due to the size , thus only for internal testing
-  #test_file("/home/wjiang2/rglab/workspace/flowCore/inst/tests/IO-testSuite.R")
+#test_file("/home/wjiang2/rglab/workspace/flowCore/tests/testthat/IO-testSuite.R")
