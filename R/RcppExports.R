d54179803684da7f05f28d3dcaf8c0160f863ead
mikejiang <wjiang2@fhcrc.org>
2017-06-02 15:16:07 -0700

    give more intuitive message when handling big FCS. #74

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,38 +2,38 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 biexponential_transform <- function(input, A, B, C, D, F, W, tol, maxIt) {
-    .Call('flowCore_biexponential_transform', PACKAGE = 'flowCore', input, A, B, C, D, F, W, tol, maxIt)
+    .Call(flowCore_biexponential_transform, input, A, B, C, D, F, W, tol, maxIt)
 }
 
 convertRawBytes <- function(bytes, isInt, colSize, ncol, isBigEndian) {
-    .Call('flowCore_convertRawBytes', PACKAGE = 'flowCore', bytes, isInt, colSize, ncol, isBigEndian)
+    .Call(flowCore_convertRawBytes, bytes, isInt, colSize, ncol, isBigEndian)
 }
 
 fcsTextParse <- function(txt, emptyValue) {
-    .Call('flowCore_fcsTextParse', PACKAGE = 'flowCore', txt, emptyValue)
+    .Call(flowCore_fcsTextParse, txt, emptyValue)
 }
 
 hyperlog_transform <- function(input, T, W, M, A, isInverse) {
-    .Call('flowCore_hyperlog_transform', PACKAGE = 'flowCore', input, T, W, M, A, isInverse)
+    .Call(flowCore_hyperlog_transform, input, T, W, M, A, isInverse)
 }
 
 inPolygon <- function(data, vertices) {
-    .Call('flowCore_inPolygon', PACKAGE = 'flowCore', data, vertices)
+    .Call(flowCore_inPolygon, data, vertices)
 }
 
 inPolytope <- function(data, A, b) {
-    .Call('flowCore_inPolytope', PACKAGE = 'flowCore', data, A, b)
+    .Call(flowCore_inPolytope, data, A, b)
 }
 
 logicle_transform <- function(input, T, W, M, A, isInverse) {
-    .Call('flowCore_logicle_transform', PACKAGE = 'flowCore', input, T, W, M, A, isInverse)
+    .Call(flowCore_logicle_transform, input, T, W, M, A, isInverse)
 }
 
 sortBytes <- function(bytes, byte_order) {
-    .Call('flowCore_sortBytes', PACKAGE = 'flowCore', bytes, byte_order)
+    .Call(flowCore_sortBytes, bytes, byte_order)
 }
 
 uint2double <- function(input, isBigEndian) {
-    .Call('flowCore_uint2double', PACKAGE = 'flowCore', input, isBigEndian)
+    .Call(flowCore_uint2double, input, isBigEndian)
 }
 
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -133,3 +133,21 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+
+static const R_CallMethodDef CallEntries[] = {
+    {"flowCore_biexponential_transform", (DL_FUNC) &flowCore_biexponential_transform, 9},
+    {"flowCore_convertRawBytes", (DL_FUNC) &flowCore_convertRawBytes, 5},
+    {"flowCore_fcsTextParse", (DL_FUNC) &flowCore_fcsTextParse, 2},
+    {"flowCore_hyperlog_transform", (DL_FUNC) &flowCore_hyperlog_transform, 6},
+    {"flowCore_inPolygon", (DL_FUNC) &flowCore_inPolygon, 2},
+    {"flowCore_inPolytope", (DL_FUNC) &flowCore_inPolytope, 3},
+    {"flowCore_logicle_transform", (DL_FUNC) &flowCore_logicle_transform, 6},
+    {"flowCore_sortBytes", (DL_FUNC) &flowCore_sortBytes, 2},
+    {"flowCore_uint2double", (DL_FUNC) &flowCore_uint2double, 2},
+    {NULL, NULL, 0}
+};
+
+RcppExport void R_init_flowCore(DllInfo *dll) {
+    R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
+}
