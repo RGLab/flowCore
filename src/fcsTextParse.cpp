e8ea6d75bef4daf8d8ef60c03ba1a4990dd05fb8
mikejiang <wjiang2@fhcrc.org>
2017-09-28 12:33:40 -0700

    remove the dependency on regex lib

--- src/fcsTextParse.cpp
+++ src/fcsTextParse.cpp
@@ -5,9 +5,10 @@
  *      Author: wjiang2
  */
 
-#include <boost/regex.hpp>
+#include <boost/algorithm/string.hpp>
 #include <boost/lexical_cast.hpp>
 #include "pairVectorRcppWrap.h"
+using namespace std;
 //#include <Rcpp.h>
 // [[Rcpp::plugins(myRegEx)]]
 // [[Rcpp::depends(BH)]]
@@ -19,39 +20,48 @@ myPairs fcsTextParse(std::string txt, bool emptyValue){
 		/*
 		 * get the first character as delimiter
 		 */
-		std::string delimiter = txt.substr(0,1);
+		char delimiter = txt[0];
 
 		/*
 		 * check if string ends with delimiter
 		 */
-		bool isDelimiterEnd = txt.substr(txt.size()-1, 1) == delimiter;
-
-//		regexes require double-escaping (*sigh*)
-//		if(delimiter == "\\" || delimiter == "|")
-			delimiter = "\\" + delimiter;
+		bool isDelimiterEnd = txt[txt.size()-1] == delimiter;
 
 
 
 		std::string doubleDelimiter,magicString;
-		doubleDelimiter = delimiter + delimiter;
-		magicString = "\\0QuickAndDirty\\0";
-//		std::cout << doubleDelimiter << ":" << magicString <<std::endl;
-		unsigned i = 0; //counter
+		doubleDelimiter.push_back(delimiter);
+		doubleDelimiter.push_back(delimiter);
+		//search for the first unused odd char as replacememnt for double delimiter
+		//FCS 3.1 states only 0-126 ASCII are legal delimiter, but we can't assume the file always follows the standard
+		//also the TEXT main contain some special characters , thus we want to make sure the replacement char is not used anywhere in FCS TEXT
+		char oddChar = 127;
+		for(; oddChar < 256; oddChar++)
+		{
+
+			if(oddChar==delimiter||txt.find(oddChar)!=std::string::npos)
+				continue;
+			else
+				break;
+		}
+		if(oddChar==256)
+			Rcpp::stop("Can't find the unused odd character from ASCII(127-255) in FSC TEXT section!");
+
+		std::string soddChar;
+		soddChar.push_back(oddChar);
 		myPair kw;
 		/*
 		 *	when empty value is allowed, we have to take the assumption that there is no double delimiters in any keys or values,
 		 */
-		if(!emptyValue)//replace the double delimiter with a magic strings
-			txt = boost::regex_replace(txt, boost::regex(doubleDelimiter), magicString);//somehow boost::replace_all won't do the job for \\\\
-		std::cout << txt << std::endl;
+		if(!emptyValue)//replace the double delimiter with the odd char
+			boost::replace_all(txt, doubleDelimiter, soddChar);
 
-		/*
-		 * then split by single delimiter
-		 */
-		boost::sregex_token_iterator token_begin(txt.begin() + 1, txt.end(), boost::regex(delimiter), -1), token_end;
-		while(token_begin != token_end){
-			i++;
-			std::string token = *token_begin++;
+		std::vector<std::string> tokens;
+		boost::split(tokens, txt, [delimiter](char c){return c == delimiter;});
+
+		unsigned j = isDelimiterEnd?tokens.size()-2:tokens.size()-1;//last token, skip the last empty one when end with delimiter
+		for(unsigned i = 1; i <= j; i++){//counter, start with 1 to skip the first empty tokens
+			std::string token = tokens[i];
 //			std::cout << token << " ";
 			if(!emptyValue){
 				/*
@@ -59,7 +69,7 @@ myPairs fcsTextParse(std::string txt, bool emptyValue){
 				 * (this slows down things quite a bit, but still a lot faster than R version,
 				 *  and this double delimiter logic is not normally invoked anyway)
 				 */
-				token = boost::regex_replace(token, boost::regex(magicString), doubleDelimiter);
+				boost::replace_all(token, soddChar, doubleDelimiter);
 //				std::cout << token;
 			}
 //			std::cout << std::endl;
@@ -82,11 +92,11 @@ myPairs fcsTextParse(std::string txt, bool emptyValue){
 		/*
 		 * check if kw and value are paired
 		 */
-		 if(i%2 == 1){
+		 if(j%2 == 1){
 			 if(isDelimiterEnd){
 			   // Rcpp::stop
 			   std::string serror = "uneven number of tokens: ";
-			   serror.append(boost::lexical_cast<std::string>(i-1));
+			   serror.append(boost::lexical_cast<std::string>(j	));
 			   throw std::range_error(serror.c_str());
 			 }	 
 			 else
