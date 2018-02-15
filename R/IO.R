66a9afc3abfd1e10cd3c43560bc7481422d019fe
mikejiang <wjiang2@fhcrc.org>
2018-02-15 10:06:40 -0800

    coerce PnE to numeric to preserve precision

--- R/IO.R
+++ R/IO.R
@@ -229,10 +229,10 @@ makeFCSparameters <- function(cn, txt, transformation, scale, decades,
         if(any(noPnE))
             ampliPar[noPnE] <- "0,0"
         ampli <- do.call(rbind,lapply(ampliPar, function(x)
-                                        as.integer(unlist(strsplit(x,",")))))
+                                        as.numeric(unlist(strsplit(x,",")))))
         for (i in 1:npar)
             if(ampli[i,1] > 0)
-                range[,i] <- 10^((range[,i]/(origRange[i]-1))*ampli[i,1])
+                range[,i] <- 10^(range[,i]/(origRange[i]-1)*ampli[i,1])**ampli[i,2]
     }
     else if(scale)
         range[2,] <- rep(10^decades, npar)
@@ -920,7 +920,7 @@ readFCSdata <- function(con, offsets, x, transformation, which.lines,
           ampliPar[noPnE] <- "0,0"
        }
        ampli <- do.call(rbind,lapply(ampliPar, function(x)
-                   as.integer(unlist(strsplit(x,",")))))
+                   as.numeric(unlist(strsplit(x,",")))))
        PnGPar <- readFCSgetPar(x, paste("$P", 1:nrpar, "G", sep=""), strict=FALSE)
        noPnG <- is.na(PnGPar)
        if(any(noPnG)) PnGPar[noPnG] <- "1"
@@ -941,7 +941,7 @@ readFCSdata <- function(con, offsets, x, transformation, which.lines,
              # dat[,i] <- 10^((dat[,i]/range[i])*ampli[i,1])
              # M.Jiang, Jan 28, 2018. Based on the standard, formula should be  xs = 10^(f1 * xc /(r)) * f2.
              dat[,i] <- 10^((dat[,i]/range[i])*ampli[i,1])*ampli[i,2]
-             range[i] <- 10^ampli[i,1]
+             range[i] <- 10^ampli[i,1]*ampli[i,2]
           }
           else if (fcsPnGtransform && PnGPar[i] != 1) {
              dat[,i] <- dat[,i] / PnGPar[i]
