5b4f45f6d4aa7ad0ba7f21ca14481ad73a38f4ed
mikejiang <wjiang2@fhcrc.org>
2019-05-08 10:55:43 -0700

    fix #86

--- src/inPolygon.cpp
+++ src/inPolygon.cpp
@@ -35,6 +35,10 @@ void inPolygon_c(double *data, int nrd,
   int i, j, counter;
   double xinters;
   double p1x, p2x, p1y, p2y;
+  //find max py
+  double p_y_max = vertices[nrv];
+  for(i = 1; i < nrv; i++)
+	  p_y_max = max(vertices[i + nrv], p_y_max);
 
   for(i=0; i<nrd; i++)
   {//iterate over points
@@ -55,22 +59,50 @@ void inPolygon_c(double *data, int nrd,
       }//else
       /*if horizontal ray is in range of vertex find the x coordinate where
 	ray and vertex intersect*/
-      if(data[i+nrd] >= min(p1y, p2y) && data[i+nrd] <= max(p1y, p2y) &&
-         data[i] <= max(p1x, p2x) && p2y != p1y)
+      double p_y_bottom = p1y;
+      double p_y_top = p2y;
+      bool is_p2_top = true;
+      if(p_y_bottom > p_y_top)
       {
-  	      xinters = (data[i+nrd]-p1y)*(p2x-p1x)/(p2y-p1y)+p1x;
+    	  std::swap(p_y_bottom, p_y_top);
+    	  is_p2_top = false;
+      }
+      double p_x_left = p1x;
+      double p_x_right = p2x;
+      if(p_x_left > p_x_right)
+    	  std::swap(p_x_left, p_x_right);
+      double data_x = data[i];
+      double data_y = data[i + nrd];
+      if(data_y >= p_y_bottom && data_y < p_y_top &&
+         data_x <= p_x_right && p2y != p1y)
+      {
+  	      xinters = (data_y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x;
   	      /*if intersection x coordinate == point x coordinate it lies on the
   	      boundary of the polygon, which means "in"*/
-        	if(xinters==data[i])
+        	if(xinters==data_x)
       	  {
         	  counter=1;
         	  break;
         	}
         	/*count how many vertices are passed by the ray*/
-        	if (xinters > data[i])
+        	if (xinters > data_x)
       	  {
         	  counter++;
         	}
+      }
+      else if(data_y == p_y_max)//handle cell that is at the same y-level as the top vertex/edge
+      {
+    	  if(p_y_top == data_y)//one end of edge reach the same y as top
+    	  {
+    		  if(p_y_bottom == data_y)//horizontal top edge
+    		  {
+    			  counter = data_x >= p_x_left && data_x <= p_x_right;//whether on the edge
+
+    		  }
+    		  else//check if on the top vertex
+    			  counter = data_x == (is_p2_top?p2x:p1x);
+    	  }
+    	  break;
       }
       p1x=p2x;
       p1y=p2y;
