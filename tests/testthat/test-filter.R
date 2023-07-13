e2fc6f84444008107a3f8dae70241bfa1084a915
mikejiang <mike@ozette.ai>
2023-07-12 18:10:41 -0700

    remove fragile flowstats::norm2filter tests which should be here in the first place

--- tests/testthat/test-filter.R
+++ tests/testthat/test-filter.R
@@ -20,47 +20,5 @@ test_that("filter", {
       expect_equal(sum(f06 %in% filter1), 8765)
       
       
-          
-      filter2 = flowStats::norm2Filter("FSC-H","SSC-H",scale.factor=2,filterId="Live Cells")
-      b08.result2 = filter(b08,filter2 %subset% b08.result1)
-      expect_equal(filterDetails(b08.result2), expectRes[["filter.norm2Filter"]])
-      expect_equal(sum(b08 %in% (filter2 %subset% b08.result1)), 6496)
-      
-      e07.result2 = filter(e07,filter2 %subset% e07.result1)
-      expect_equal(sum(e07 %in% (filter2 %subset% e07.result1)), 6416)
-      
-      f06.result2 = filter(f06,filter2 %subset% f06.result1)
-      expect_equal(sum(f06 %in% (filter2 %subset% f06.result1)), 6959, tol = 2e-3)
-      
-      ## the third-fifth gates get the positive cells for the marker in FL1-H
-      ## this is a really interesting example because it illustrates that there
-      ## are two subpopulations. Naturally we would like to automatically find them
-      ## In this case we want to now what percent the positive population in FL1-H is of the
-      ## total population
-
-      filter3 = rectangleGate("FL1-H"=c(.4,Inf),filterId="FL1-H+")
-      b08.result3 = filter(b08,filter3 %subset% b08.result2)
-      expect_equal(summary(b08.result3),expectRes[["filter.b08.result3"]]) 
-      # An example of doing manipulations with filterResult objects 
-      # since they are now also filter objects. The %subset% argument
-      # has a special summary meaning because it does it's calculations
-      # relative to the RHS argument, whereas filterResults are ALWAYS
-      # wrt to the full dataset. So, the value below should differ from 
-      # the value above.
-      expect_equal(summary(b08.result3 %subset% b08.result2), expectRes[["filter.b08.result3.subset"]])
-
-
-      filter4 = flowStats::norm2Filter("FSC-H","FL1-H",scale.factor=2,filterId="FL1-H+")
-      b08.result4 = filter(b08,filter4 %subset% b08.result2)
-      expect_equal(summary(b08.result4),expectRes[["filter.b08.result3.subset2"]]) 
-
-      expect_equal(summary(b08.result4 %subset% b08.result2)@p, 0.4802)
-      
-      #We now have NOT gates so we can proceed!
-      b08.result5 = filter(b08,filter4 %subset% (b08.result2 & !b08.result4))
-      expect_equal(summary(b08.result5)@true, 1407)
-
-      expect_equal(summary(b08.result4 %subset% (b08.result4 | b08.result5))$p, 0.4802)
-      
     })
 
